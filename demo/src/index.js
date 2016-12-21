b158b5a54141dba4cd4d04d9c1ecb23e64ae685b
bryon-czoch <bczoch@iu.edu>
2016-12-21 13:32:02 -0500

    merged in fixed-data-table API code update

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import {render} from 'react-dom'
-
+import 'fixed-data-table/dist/fixed-data-table.css';
 import Component from '../../src'
 import faker from 'faker';
+
+
+function getHeaders(){
+
+ var columns = [
+ {id:'id', name:'ID'},
+ {id:'name', name:'Name'},
+ {id:'address', name:'Street Address'},
+ {id:'zip', name:'Zip Code'}
+ ];
+
+ return columns;
+
+}
 
 function generateList(){
   var items = [];
 
   for (var i=1; i<=5000; i++){
-      items.push({ id: i, name: faker.name.findName(), address: faker.address.streetAddress(), state: faker.address.stateAbbr(), zip: faker.address.zipCode()});
-    }
+   items.push({ id: i, name: faker.name.findName(), address: faker.address.streetAddress(), email: faker.address.stateAbbr(), zip: faker.address.zipCode()});
+ }
 
-    return items;
-  };
+ return items;
+};
 
 let Demo = React.createClass({
   render() {
     return <div>
-      <h1>Alleles</h1>
+    <h1>Alleles</h1>
 
-      <Component data={generateList()}/>
+    <Component data={generateList()} columns={getHeaders()}/>
     </div>
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,183 +1,77 @@
-import React, { PropTypes, Component } from 'react';
-import {Table, Column, Cell} from 'fixed-data-table';
-import 'fixed-data-table/dist/fixed-data-table.css';
-import faker from 'faker';
+const React = require('react');
+const {Table, Column, Cell} = require('fixed-data-table');
 
-var SortTypes = {
-  ASC: 'ASC',
-  DESC: 'DESC',
-};
-
-var FlybaseTable = React.createClass({
-  getInitialState() {
-
-   return {
-    rows: this.props.data,
-    filteredRows: null,
-    filterBy: null,
-    sortBy: 'id',
-    sortDir: null,
-  };
-},
-
-toCSV(objArray){
-  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-  var str = '';
-
-  for (var i = 0; i < array.length; i++) {
-    var line = '';
-    for (var index in array[i]) {
-      if (line != '') line += ','
-
-        line += array[i][index];
-    }
-
-    str += line + '\r\n';
-  }
-  str = Object.keys(objArray[0]) + '\n' + str;
-  return str;
-},
-
-_download(e){
-
-  const a = document.createElement('a');
-  a.textContent = 'download';
-  a.download = 'filename';
-
-  var bomCode = '%EF%BB%BF';  
-
-  var data = this.toCSV(this.state.filteredRows);
- 
-  a.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(data);
-  a.click();
-
-},
-
-componentWillMount() {
-  this._filterRowsBy(this.state.filterBy);
-},
-
-_rowGetter(rowIndex) {
-  return this.state.filteredRows[rowIndex];
-},
-
-_filterRowsBy(filterBy) {
-  var rows = this.state.rows.slice();        
-  var filteredRows = filterBy ? rows.filter(function(row){
-    return row.name.toLowerCase().indexOf(filterBy.toLowerCase()) >= 0
-  }) : rows;
-
-  this.setState({
-    filteredRows,
-    filterBy,
-  });
-},
-
-_onFilterChange(e) {
-  this._filterRowsBy(e.target.value);
-},
-
-_sortRowsBy(cellDataKey) {
-  var sortDir = this.state.sortDir;
-  var sortBy = cellDataKey;
-  if (sortBy === this.state.sortBy) {
-    sortDir = this.state.sortDir === SortTypes.ASC ? SortTypes.DESC : SortTypes.ASC;
-  } else {
-    sortDir = SortTypes.DESC;
-  }
-
-  var rows = this.state.filteredRows.slice();
-  rows.sort(function(a, b){
-    var sortVal = 0;
-    if (a[sortBy] > b[sortBy]) {
-      sortVal = 1;
-    }
-    if (a[sortBy] < b[sortBy]) {
-      sortVal = -1;
-    }
-
-    if (sortDir === SortTypes.DESC) {
-      sortVal = sortVal * -1;
-    }
-
-    return sortVal;
-  });
-
-  this.setState({
-    filteredRows: rows,
-    sortBy,
-    sortDir,
-  });
-},
-
-_renderHeader(label, cellDataKey) {
-  return (
-    <a href="#" onClick={this._sortRowsBy.bind(null, cellDataKey)}>{label}</a>
-
-    );
-  },
-  
-  render() {   
-    var sortDirArrow = "";
-    
-    if (this.state.sortDir !== null){
-      sortDirArrow = this.state.sortDir === SortTypes.DESC ? ' ↓' : ' ↑';
-    }
-    
+class MyColumnCell extends React.Component {
+  render() {
+    const {headers, rowIndex, field, data, ...props} = this.props;
     return (
-    <div>
+      <Cell {...props}>
+      {data[rowIndex][field]}
+      </Cell>
+      );
+    }
+  }
 
-    <label>Filter by <input onChange={this._onFilterChange} /></label>
+  class MyTextCell extends React.Component {
+    render() {
+      const {rowIndex, field, data, ...props} = this.props;
+      return (
+      <Cell {...props}>
+      {data[rowIndex][field]}
+      </Cell>
+      );
+    }
+  }
 
-    <button onClick={this._download}> Download </button>
+  class MyLinkCell extends React.Component {
+    render() {
+      const {rowIndex, field, data, ...props} = this.props;
+      const link = data[rowIndex][field];
+      return (
+      <Cell {...props}>
+      <a href={link}>{link}</a>
+      </Cell>
+      );
+    }
+  }
 
-    <Table
-    rowHeight={30}
-    rowGetter={this._rowGetter}
-    rowsCount={this.state.filteredRows.length}
-    width={700}
-    maxHeight={1000}
-    headerHeight={40}>
+  class MyTable extends React.Component {
+    constructor(props) {
+      super(props);
 
-    <Column
-    headerRenderer={this._renderHeader}
-    label={'id' + (this.state.sortBy === 'id' ? sortDirArrow : '')}
-    width={100}
-    dataKey="id"
-    />
+      this.state = {
+        myTableData: this.props.data
+      };
+    }
 
-    <Column
-    headerRenderer={this._renderHeader}
-    label={'Name' + (this.state.sortBy === 'name' ? sortDirArrow : '')}
-    width={200}
-    dataKey="name"
-    />
+    render() {
 
-    <Column
-    headerRenderer={this._renderHeader}
-    label={'Address' + (this.state.sortBy === 'address' ? sortDirArrow : '')}
-    width={200}
-    dataKey="address"
-    />
+      const columns = this.props.columns.map((column) =>
+      <Column
+      header={<Cell>{column.name}</Cell>}
+      cell={
+        <MyTextCell
+        data={this.state.myTableData}
+        field={column.id}
+        />
+      }
+      width={200}
+      />
+      );
 
-    <Column
-    headerRenderer={this._renderHeader}
-    label={'State' + (this.state.sortBy === 'state' ? sortDirArrow : '')}
-    width={100}
-    dataKey="state"
-    />
+      return (
+      <Table
+        rowsCount={this.state.myTableData.length}
+        rowHeight={50}
+        headerHeight={50}
+        width={1000}
+        height={500}>
 
-    <Column
-    headerRenderer={this._renderHeader}
-    label={'Zip' + (this.state.sortBy === 'zip' ? sortDirArrow : '')}
-    width={100}
-    dataKey="zip"
-    />
+        {columns}
+        
+      </Table>
+      );
+    }
+  }
 
-    </Table>
-    </div>
-    );
-  },
-});
-
-export default FlybaseTable;
+  export default MyTable;
