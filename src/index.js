6a75e38ef6d34b91a87c95ac493c8c8249f4ac01
Josh Goodman <jogoodma@indiana.edu>
2017-01-05 12:27:52 -0500

    Implemented sort and added base code for filtering.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import FixedDataTable, { Table, Column, Cell } from 'fixed-data-table';
+import _ from 'underscore';
 
 import { ASC, DESC, NONE } from './constants';
 import Header from './components/Header';
@@ -7,21 +8,42 @@ import Header from './components/Header';
 class FlyBaseDataGrid extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sortDir: this.initSortCols(),
+      items: props.data,
+      filter: '',
+    };
+    this.handleSort   = this.handleSort.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
+  initSortCols() {
     const sortCols = {};
     this.props.columns.forEach((col) => sortCols[col.id] = NONE);
+    return sortCols;
+  }
 
-    this.state = {
-      sortDir: sortCols,
-    };
-    this.handleSort = this.handleSort.bind(this);
+  handleFilter(e) {
+    const filterText = e.target.value;
+    // Implement filter logic here by expanding the callback inside the filter.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    const filteredItems = this.state.items.filter((item) => { return true; });
+    this.setState({items: filteredItems, filter: filterText});
   }
 
   handleSort(column) {
-    const current = this.state.sortDir;
+    let current = this.state.sortDir;
     const sortDir = current[column];
 
-    this.props.columns.forEach((col) => current[col.id] = NONE);
+    let sortedItems = _.sortBy(this.state.items, column);
+    if ( sortDir === ASC) {
+      sortedItems.reverse();
+    }
+
+    // Reset all column sorts before we set the new sort direction below.
+    current = this.initSortCols();
+
+    // Reverse sort
     if (sortDir === ASC) {
       current[column] = DESC;
     }
@@ -29,14 +51,26 @@ class FlyBaseDataGrid extends Component {
       current[column] = ASC;
     }
     
-    this.setState({sortDir: current});
+    // Set state with new sorted order.
+    this.setState({
+      sortDir: current,
+      items: sortedItems,
+    });
   }
 
   render() {
-    const { data, columns, ...props } = this.props;
+    const { data, columns, showFilter, ...props } = this.props;
+    const { items } = this.state;
     return (
-      <Table {...props}>
-      {columns.map((column) => 
+      <div>
+      {/*
+         Add filter box here.  Start with one input box that filters all columns.
+         The filter box should be a custom component.
+         e.g.
+         { showFilter && <Filter value={this.state.filter} onChange={this.handleFilter} /> }
+      */}
+        <Table {...props}>
+          {columns.map((column) => 
                    <Column
                      key={column.id}
                      columnKey={column.id} 
@@ -49,15 +83,16 @@ class FlyBaseDataGrid extends Component {
                      }
                      cell={props => (
                        <Cell {...props}>
-                         {data[props.rowIndex][column.id]}
+                         {items[props.rowIndex][column.id]}
                        </Cell>
                      )
                      }
                      width={200}
                    />
                   )
-      }
-      </Table>
+          }
+        </Table>
+      </div>
     );
   }
 }
@@ -76,6 +111,7 @@ FlyBaseDataGrid.propTypes = {
   headerHeight: PropTypes.number,
   width: PropTypes.number,
   height: PropTypes.number,
+  showFilter: PropTypes.bool,
 };
 
 FlyBaseDataGrid.defaultProps = {
@@ -83,6 +119,7 @@ FlyBaseDataGrid.defaultProps = {
   headerHeight: 50,
   width: 1000,
   height: 500,
+  showFilter: false,
 };
 
 export default FlyBaseDataGrid;
