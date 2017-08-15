fc21d202906cff0ec0e60afa0b0ca77d3809cf7c
Bryon Czoch <bczoch@iu.edu>
2017-08-15 16:05:50 -0400

    add dynamic placeholder text

--- src/components/ColumnFilter/index.js
+++ src/components/ColumnFilter/index.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import ContainerDimensions from 'react-container-dimensions'
 
 function Filter(props) {
-  const { value, onChange } = props;
+  const { value, onChange, placeholder } = props;
 
   return (
   	<ContainerDimensions>
-      { ({ width }) =>   <input onChange={onChange} placeholder={''} style={{width:width-10}} /> }
+      { ({ width }) =>   <input onChange={onChange} placeholder={placeholder} style={{width:width-10}} /> }
     </ContainerDimensions>    
   );
 }
@@ -17,6 +17,10 @@ function Filter(props) {
  Filter.propTypes = {
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
+ };
+
+ Filter.defaultProps = {
+  placeholder: '',
  };
 
 export default Filter;
